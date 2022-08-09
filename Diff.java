19a76233f0083721ab0440b65cf0989f02a925aa
Vinit Variyani <vinitvaryani123@gmail.com>
2022-08-09 15:41:02 +0530

    Updation Done Successfully

--- Diff.java
+++ Diff.java
@@ -3,7 +3,8 @@ public class Diff {
     public static void main(String[] args){
         int a = 10;
         int b = 5;
+        int c = 6;
 
-        System.out.println("Diff is "+(a-b));
+        System.out.println("Diff is "+(a-b+c));
     }
 }
